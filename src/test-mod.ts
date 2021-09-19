117584b024c70ade3f0ff9353a185db86c985584
TPHRyan <ryan.paroz@gmail.com>
2021-09-19 15:18:01 +1000

    Tweak overlay to work better with dev tools

--- src/test-mod.ts
+++ src/test-mod.ts
@@ -1,24 +1,11 @@
-import { createElement, createTextNode, defineMod } from "../kiln-helpers";
+import { defineMod } from "../kiln-helpers";
 
 export const testMod = defineMod((ctx) =>
-	ctx
-		.hook("init", function () {
-			const testOverlayElement = createElement("div");
-			testOverlayElement.setAttribute("id", "overlay-test");
-			testOverlayElement.appendChild(createTextNode("Hello, world!"));
-			this.addOverlayElement(testOverlayElement);
-			this.addStyles(`
-			#overlay-test {
-				margin-left: 5px;
-				margin-top: 5px;
-			}
-		`);
-		})
-		.hook("ticker", (Game) => [
-			`You have registered ${
-				Object.keys(Game.kilnData.hooks).length
-			} kinds of hooks!`,
-		]),
+	ctx.hook("ticker", (Game) => [
+		`You have registered ${
+			Object.keys(Game.kilnData.hooks).length
+		} kinds of hooks!`,
+	]),
 );
 
 export default testMod;
