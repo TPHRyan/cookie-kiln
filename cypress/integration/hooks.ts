44e540b5a84707610da9e7c6867418887d8bdb20
TPHRyan <ryan.paroz@gmail.com>
2021-09-25 13:18:35 +1000

    Add test for 'check' hook

--- cypress/integration/hooks.ts
+++ cypress/integration/hooks.ts
@@ -1,4 +1,34 @@
 describe("Kiln Hooks", () => {
+	it("Can register a working 'check' hook", () => {
+		const updatedCookieCount = 123456;
+		const formattedCookieCount = new Intl.NumberFormat().format(
+			updatedCookieCount,
+		);
+		const checkMod: CookieKiln.Mod = (ctx) =>
+			ctx.hook("check", (Game) => {
+				Game.cookies = updatedCookieCount;
+			});
+
+		cy.cookieClicker();
+		cy.window().should("have.property", "Game").as("Game");
+		cy.get<Game>("@Game").then((Game: Game) => {
+			Game.registerKilnMod("checkMod", checkMod);
+			cy.get<Game>("@Game")
+				.should("have.property", "kilnData")
+				.and("have.property", "mods")
+				.and("have.property", "checkMod");
+			cy.get<Game>("@Game").then((Game) => {
+				const divisor = Game.fps * 5;
+				const nextCheckT =
+					Game.T * (Math.ceil(Game.T / divisor) * divisor);
+				Game.T = nextCheckT - 100;
+				cy.get("@Game")
+					.should("have.property", "T")
+					.and("be.gte", nextCheckT);
+				cy.get("#cookies").contains(`${formattedCookieCount} cookies`);
+			});
+		});
+	});
 	it("Can register a working 'ticker' hook", () => {
 		const tickerText = "Hello, world!";
 		const tickerMod: CookieKiln.Mod = (ctx) =>
